41d1984cb92962c7ef7a7df2f90be50e67fbf1ea
sejal <sejalvishnoi32@gmail.com>
2023-04-08 09:50:30 +0530

    commit message

---
+++ exercise.py
@@ -0,0 +1,5 @@
+# create a dictinary and take input form the user and retrune the 
+# meaning of the word from the dictinary
+d1 = {"sejal":"02/01/2006","prince":"10/07/2008","password":"seju1234"}
+name = input("enter the name:")
+print(d1[name]) 
